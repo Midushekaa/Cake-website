a0334884dd660662becb5c73147891e24f92e6c7
Midushekaa <abe.midu@gmail.com>
2026-02-20 10:24:19 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,63 +1,35 @@
 // Navbar Toggle
-let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-bar').onclick = () => {
-    navbar.classList.toggle('active');
+    document.querySelector('.navbar').classList.toggle('active');
 };
 
 // Search Toggle
-let search = document.querySelector('.search');
 document.querySelector('#search').onclick = () => {
-    search.classList.toggle('active');
+    document.querySelector('.search').classList.toggle('active');
 };
 
 // Product Swiper
 new Swiper(".product-row", {
     spaceBetween: 30,
     loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".product .swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        0: { slidesPerView: 1 },
-        768: { slidesPerView: 2 },
-        1024: { slidesPerView: 3 },
-    },
+    autoplay: { delay: 3000, disableOnInteraction: false },
+    pagination: { el: ".product .swiper-pagination", clickable: true },
+    breakpoints: { 0: { slidesPerView: 1 }, 768: { slidesPerView: 2 }, 1024: { slidesPerView: 3 } },
 });
 
 // Blogs Swiper
 new Swiper(".blogs-row", {
     spaceBetween: 30,
     loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".blogs .swiper-pagination",
-        clickable: true,
-    },
+    autoplay: { delay: 3000, disableOnInteraction: false },
+    pagination: { el: ".blogs .swiper-pagination", clickable: true },
 });
 
 // Review Swiper
 new Swiper(".review-row", {
     spaceBetween: 30,
     loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".review .swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        0: { slidesPerView: 1 },
-        768: { slidesPerView: 2 },
-        1024: { slidesPerView: 3 },
-    },
+    autoplay: { delay: 3000, disableOnInteraction: false },
+    pagination: { el: ".review .swiper-pagination", clickable: true },
+    breakpoints: { 0: { slidesPerView: 1 }, 768: { slidesPerView: 2 }, 1024: { slidesPerView: 3 } },
 });
